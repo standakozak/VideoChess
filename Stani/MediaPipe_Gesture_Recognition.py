c5f5bfbf5014777b947b557e9251ec40cbfa892c
standakozak <standa23k@seznam.cz>
2024-05-22 22:53:10 +0200

    add gesture recognition in all orientations

--- Stani/MediaPipe_Gesture_Recognition.py
+++ Stani/MediaPipe_Gesture_Recognition.py
@@ -6,9 +6,15 @@ described at https://developers.google.com/mediapipe/solutions/vision/gesture_re
 ### IMPORTS
 
 import cv2
+import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+
+mp_hands = mp.solutions.hands
+
+from cv2.typing import MatLike
+
 
 ### CONSTANTS
 
@@ -73,6 +79,52 @@ def get_gesture_and_state(recognition_result) -> GestureState:
     return GestureState(HandGestureState.NONE, "")
 
 
+def get_index_finger_top_and_bottom(image: MatLike, hands: mp.solutions.hands.Hands):
+    h, w, _ = image.shape
+
+    results = hands.process(image)
+    if results.multi_hand_landmarks:
+        hand_landmarks = results.multi_hand_landmarks[0]
+        index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+        index_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
+
+        tip_ix, tip_iy = int(index_tip.x * w), int(index_tip.y * h)
+        mcp_ix, mcp_iy = int(index_mcp.x * w), int(index_mcp.y * h)
+
+        return (tip_ix, tip_iy), (mcp_ix, mcp_iy)
+    
+def get_angle_between_vectors(v, w):
+    return np.arccos(v.dot(w) / np.linalg.norm(v) * np.linalg.norm(w))
+
+def rotate_image(image, angle, rotation_center):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    #rot_mat = cv2.getRotationMatrix2D(tuple(rotation_center.astype(int)), angle, 1.0)
+    
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+
+def rotate_by_index_finger(image: MatLike, hands: mp.solutions.hands.Hands) -> MatLike:
+    index_finger_landmarks = get_index_finger_top_and_bottom(image, hands)
+    if index_finger_landmarks is not None:
+        index_top = np.array((index_finger_landmarks[0][0], index_finger_landmarks[0][1]))
+        index_bottom = np.array((index_finger_landmarks[1][0], index_finger_landmarks[1][1]))
+
+        finger_vector = index_top - index_bottom
+        basis_vector = np.array((0, -1))
+
+        angle = get_angle_between_vectors(finger_vector, basis_vector)
+
+        # -1 -> must be rotated clockwise, 1 -> must be rotated counter-clockwise
+        rotation_orientation = int(np.cross(finger_vector, basis_vector) <= 0) * 2 - 1
+        
+        angle = np.rad2deg(angle * rotation_orientation)
+        
+        rotated_image = rotate_image(image, angle, index_top)
+        return rotated_image
+
+
 if __name__ == "__main__":
     base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
     
@@ -84,35 +136,44 @@ if __name__ == "__main__":
 
     last_gesture_state = get_gesture_and_state(None)
 
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            continue
-        
-        # Convert the image from BGR to RGB
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=1, model_complexity=0) as hands:
 
-        # Flipping the image
-        image = cv2.flip(image, 1)
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                continue
+            
+            # Convert the image from BGR to RGB
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Process the image
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-        recognition_result = recognizer.recognize(mp_image)
+            # Flipping the image
+            image = cv2.flip(image, 1)
+            image_to_process = image
+            image_to_show = image
 
-        current_gesture_state = get_gesture_and_state(recognition_result)
-        resolve_hand_gesture_state_change(current_gesture_state, last_gesture_state)
+            rotated_image = rotate_by_index_finger(image, hands)
+            
+            if rotated_image is not None:
+                image_to_process = rotated_image
 
-        # Convert the image back to BGR for displaying
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                # Test if the image is rotated properly:
+                #image_to_show = rotated_image
 
-        # Flip the image
-        image = cv2.flip(image, 1)
+            # Process the image
+            mp_image: mp.Image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_to_process)
+            recognition_result = recognizer.recognize(mp_image)
 
-        # Display the annotated image
-        cv2.imshow('Hand Tracking', image)
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
-        
-        last_gesture_state = current_gesture_state
+            current_gesture_state = get_gesture_and_state(recognition_result)
+            resolve_hand_gesture_state_change(current_gesture_state, last_gesture_state)
+            
+            # Convert the image back to BGR for displaying
+            image_to_show = cv2.cvtColor(image_to_show, cv2.COLOR_RGB2BGR)
 
-    cap.release()
+            # Display the annotated image
+            cv2.imshow('Hand Tracking', image_to_show)
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+            
+            last_gesture_state = current_gesture_state
+
+        cap.release()
